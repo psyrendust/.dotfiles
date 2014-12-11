a9ab8ebab0500aa690d4661d15c773e7a3cce3d9
Larry Gordon <lgordon@psyrendust.com>
2014-12-10 18:03:10 -0800

    Updates bootstrap.sh script

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Setup
 #
@@ -9,6 +9,10 @@ if [ ! -n "$ZSH_CUSTOM" ]; then
   ZSH_CUSTOM=~/.dotfiles
 fi
 
-for script in $ZSH_CUSTOM/{homebrew,ohmyzsh,config}/install.sh; do
+export PATH="$ZSH_CUSTOM/plugins/pretty-print/bin:$PATH"
+
+ppinfo 'installing dependencies'
+
+for script in $ZSH_CUSTOM/{homebrew,ohmyzsh,templates}/install.sh; do
   "$script" 2>&1
 done
