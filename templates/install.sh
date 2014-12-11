6ec03cead2e68b7047402b2bebf4816c309950e2
Larry Gordon <lgordon@psyrendust.com>
2014-12-10 18:00:34 -0800

    Updates to templates

---
+++ templates/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+#
+# config
+#
+# This symlinks config and dotfiles to the $HOME directory
+
+ppinfo 'Install config files'
+
+setopt EXTENDED_GLOB
+local config_path=${HOME}/.dotfiles/templates
+
+for file in ${config_path}/^install.sh(.N); do
+  ln -sf "$file" "${HOME}/.${file:t}"
+done
+
+ppok ' - config install complete'
+exit 0
