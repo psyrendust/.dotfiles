6f78f510fc302d827c5716bc6ffcfa31d685c314
Larry Gordon <lgordon@psyrendust.com>
2014-12-10 18:01:46 -0800

    Adds ohmyzsh install script

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # oh-my-zsh
 #
 # This installs oh-my-zsh
 
-# Check for oh-my-zsh
+ppinfo 'Install oh-my-zsh'
+
+ppinfo ' - Check for oh-my-zsh'
 if [ ! -n "$ZSH" ]; then
   ZSH=~/.oh-my-zsh
 fi
 
 if [ ! -d "$ZSH" ]; then
-  echo "  Installing oh-my-zsh for you."
+  ppinfo " - Installing oh-my-zsh"
   curl -L http://install.ohmyz.sh | sh
+  rm ~/.zshrc
 fi
 
+ppok ' - oh-my-zsh install complete'
 exit 0
