eb02c2701b0adc5c71844d642f739cafad190fea
Larry Gordon <lgordon@psyrendust.com>
2014-12-10 18:00:47 -0800

    Adds homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,30 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Homebrew
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
+ppinfo 'Install Homebrew'
+ppinfo ' - Check for Homebrew'
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
+  ppinfo " - Installing Homebrew for you"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
-# Update the formulae and Homebrew
+ppinfo ' - Update the formulae and Homebrew'
 brew update
 
-# Upgrade outdated formulae
+ppinfo ' - Upgrade outdated formulae'
 brew upgrade
 
-# Audits your installation for common issues
+ppinfo ' - Audits your installation for common issues'
 brew doctor
 
-# Install homebrew packages
+ppinfo ' - Install homebrew packages'
 brew install coreutils git git-flow node bash zsh fasd
 
-# Uninstall unused and old versions of packages
+ppinfo ' - Uninstall unused and old versions of packages'
 brew cleanup
 
+ppok ' - Homebrew install complete'
 exit 0
