daa2ace0a1b17afb2f967f00ae47713a80eab369
Larry Gordon <lgordon@psyrendust.com>
2015-01-09 15:43:15 -0800

    Update ruby setup

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 #
-# RVM and Ruby Setup
+# rvm and Ruby Setup
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Ruby.
 
-ppinfo 'Install Ruby'
-ppinfo ' - Check for RVM'
-if [ ! -d "$HOME/.rvm" ]; then
+ppinfo 'Install rvm'
+ppinfo ' - Check for rvm'
+if test ! $(which rvm)
+then
   ppinfo " - Installing RVM"
   curl -sSL https://get.rvm.io | bash -s stable --rails
 fi
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 ppok ' - Ruby install complete'
 exit 0
