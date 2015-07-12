b00b72fdfaba1343d6de38931b695b19e61c5cbb
Larry Gordon <lgordon@psyrendust.com>
2015-07-12 16:17:31 -0700

    Fixes path issue with templates init script

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -16,10 +16,25 @@ if [ ! -n "$ZSH_CUSTOM" ]; then
   ZSH_CUSTOM=~/.dotfiles
 fi
 
-export PATH="$ZSH_CUSTOM/plugins/pretty-print/bin:$PATH"
+newpaths=(
+  "/usr/local/bin"
+  "/usr/local/opt/go/libexec/bin"
+  "/usr/local/opt/coreutils/libexec/gnubin"
+  "$ZSH_CUSTOM/bin"
+  "$ZSH_CUSTOM/plugins/npmlist/bin"
+  "$ZSH_CUSTOM/plugins/sublime/bin"
+  "$ZSH_CUSTOM/plugins/pretty-print/bin"
+  "$ZSH_CUSTOM/plugins/safe-rm/bin"
+  "$ZSH_CUSTOM/plugins/asciinema2gif/bin"
+)
+for newpath in ${newpaths[@]}; do
+  export PATH="$newpath:$PATH"
+done
 
 ppinfo "Installing dependencies"
 
-for script in $ZSH_CUSTOM/setup/{homebrew,ohmyzsh,rvm,templates,nvm}/install.sh; do
+for script in $ZSH_CUSTOM/setup/{homebrew,ohmyzsh,rvm,nvm}/install.sh; do
   "$script" 2>&1
 done
+
+$ZSH_CUSTOM/templates/install.sh 2>&1
