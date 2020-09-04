3d9659416ded8b909dc700acdea3245df28656ca
Larry Gordon <larry@visible.com>
2020-09-04 13:26:53 -0600

    feat: add prettier and semantic-release configs

---
+++ .prettierrc.js
@@ -0,0 +1,61 @@
+/**
+ * Defaults from Prettier.
+ * @see https://prettier.io/docs/en/options.html
+ */
+const prettierDefaults = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  rangeEnd: Infinity,
+  rangeStart: 0,
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+};
+
+/**
+ * Custom overrides.
+ */
+const prettierCustom = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
+
+const prettierCustomOverrides = {
+  overrides: [
+    {
+      files: ['.eslintrc', '.babelrc'],
+      options: {
+        parser: 'json',
+        semi: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: ['.editorconfig'],
+      options: {
+        parser: 'yaml',
+      },
+    },
+  ],
+};
+
+/**
+ * Prettier defaults with Custom overrides.
+ */
+const config = {
+  ...prettierDefaults,
+  ...prettierCustom,
+  ...prettierCustomOverrides,
+};
+
+module.exports = config;
---
+++ .releaserc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+    { name: 'semantic-release', prerelease: true },
+  ],
+  parserOpts: {
+    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'prettier ./CHANGELOG.md --write',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+  preset: 'conventionalcommits',
+};
