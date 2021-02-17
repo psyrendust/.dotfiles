ade22d50c35f256f56d914074912dc2edf97a66c
Larry Gordon <lgordon@twitter.com>
2021-02-17 13:15:09 -0700

    feat(plugins): add fzf plugin

---
+++ plugins/fzf/fzf.plugin.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+#
+# Custom fzf config
+# 
+# @see https://github.com/junegunn/fzf#settings
+#
+# Author:
+#   Larry Gordon
+#
+# License:
+#   The MIT License (MIT) <http://psyrendust.mit-license.org/2014/license.html>
+# ------------------------------------------------------------------------------
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
