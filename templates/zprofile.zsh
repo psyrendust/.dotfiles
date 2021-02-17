50e85204b5bb691ab7c87e0916fb1be57d57068b
Larry Gordon <lgordon@twitter.com>
2021-02-17 13:27:35 -0700

    feat(templates): add zprofile.zsh template

---
+++ templates/zprofile.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+#
+# Define executable commands that do not change the shell environment.
+#
+# Authors:
+#   Larry Gordon
+#
+# Usage: save to ~/.zshenv
+#   #!/usr/bin/env zsh
+#   zdot load "$ZSH_CUSTOM_TEMPLATES/zprofile.zsh"
+#
+# Execution Order
+#   https://github.com/psyrendust/.dotfiles/blob/master/templates/README.md#for-zsh
+#
+# License:
+#   The MIT License (MIT) <http://psyrendust.mit-license.org/2021/license.html>
+# ------------------------------------------------------------------------------
