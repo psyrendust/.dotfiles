5dea00c49f6deeb147eec11ea6b04a1475877259
Larry Gordon <lgordon@twitter.com>
2021-02-17 13:26:11 -0700

    feat(templates): add zsh extension and normalize pattern

---
+++ templates/zlogin.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+#
+# Define aliases, functions, shell options, and key bindings.
+#
+# Authors:
+#   Larry Gordon
+#
+# Usage: save to ~/.zshenv
+#   #!/usr/bin/env zsh
+#   zdot load "$ZSH_CUSTOM_TEMPLATES/zlogin.zsh"
+#
+# Execution Order
+#   https://github.com/psyrendust/.dotfiles/blob/master/templates/README.md#for-zsh
+#
+# License:
+#   The MIT License (MIT) <http://psyrendust.mit-license.org/2021/license.html>
+# ------------------------------------------------------------------------------
