0c90d934d4348197cb661f527484ca4b409ca377
Larry Gordon <lgordon@psyrendust.com>
2023-07-11 12:28:26 -0500

    feat(zshrc): add template for zshrc file

---
+++ templates/zshrc.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+#
+# Define aliases, functions, shell options, and key bindings.
+#
+# Authors:
+#   Larry Gordon
+#
+# Usage: save to ~/.zshrc
+#   #!/usr/bin/env zsh
+#   zdot load "$ZDOT_TEMPLATES/zshrc.zsh"
+#
+# Execution Order
+#   https://github.com/psyrendust/.dotfiles/blob/master/templates/README.md#for-zsh
+#
+# License:
+#   The MIT License (MIT) <http://psyrendust.mit-license.org/2021/license.html>
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Load framework
+# ------------------------------------------------------------------------------
+zdot load "$ZDOT_TEMPLATES/zshrc-zplug.zsh"
