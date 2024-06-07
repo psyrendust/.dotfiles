d75059e1907f1f99965cf67d8700e7f3249087aa
Larry Gordon <larrygordon@apple.com>
2024-06-07 14:10:12 -0500

    build(release): do not write to git on release

--- .releaserc.js
+++ .releaserc.js
@@ -14,26 +14,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: 'prettier ./CHANGELOG.md --write',
-      },
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
     '@semantic-release/github',
-    '@semantic-release/git',
   ],
   preset: 'conventionalcommits',
 };
