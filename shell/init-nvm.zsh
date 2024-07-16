2e8d8e4fba98fe8edb5e849a08da3384eb27748b
Larry Gordon <larrygordon@apple.com>
2024-07-16 16:24:15 -0500

    fix(shell): update antidote to work with zsh-z

--- shell/init-nvm.zsh
+++ shell/init-nvm.zsh
@@ -5,6 +5,6 @@
 # ------------------------------------------------------------------------------
 export NVM_DIR="$HOME/.nvm"
 export NVM_COMPLETION=true
-export NVM_LAZY_LOAD=true
+export NVM_LAZY_LOAD=false
 export NVM_NO_USE=false
 export NVM_AUTO_USE=false
